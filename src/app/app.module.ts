2fda0565d6e378990e2b159318ec8dc19e9d4895
Szerszen1 <jakubmarekszerszen@gmail.com>
2018-04-16 18:49:21 +0200

    add firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { AngularFireModule } from 'angularfire2';
-import { AngularFireDatabaseModule } from 'angularfire2/database';
-import { AngularFireAuthModule } from 'angularfire2/auth';
-import { environment } from './../environments/environment';
 
 import { AppComponent } from './app.component';
 
@@ -17,9 +13,6 @@ import { AppComponent } from './app.component';
     BrowserModule,
     FormsModule,
     HttpModule
-    AngularFireModule.initializeApp(environment.firebase),
-    AngularFireDatabaseModule,
-    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
