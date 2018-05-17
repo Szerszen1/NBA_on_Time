a7485313b986427a5f23a8260e87dc77d5617cb6
Daooshi <dominikwozniczka96@gmail.com>
2018-05-17 21:26:33 +0200

    routing for youtube-player in app-routing.module.ts added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserProfileComponent } from './user-profile/user-profile.component';
 import { AuthGuard } from './core/auth.guard';
 import { UserFormComponent } from './user-form/user-form.component';
+import { YoutubePlayerComponent} from './youtube-player/youtube-player.component';
 
 const routes: Routes = [
   { path: 'user-profile', component: UserProfileComponent},
   { path: 'user-form', component: UserFormComponent},
+  { path: 'youtube-player', component: YoutubePlayerComponent},
 ];
 
 @NgModule({
