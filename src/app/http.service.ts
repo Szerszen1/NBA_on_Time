40b3e0d7523beb8673b96b50886eba36b169105b
Szerszen1 <jakubmarekszerszen@gmail.com>
2018-06-01 14:07:02 +0200

    add http service

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpParams, HttpClient } from '@angular/common/http';
+import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Authorization': 'Szerszen1:szerszen1' })
+};
 
 @Injectable()
 export class HttpService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  get() {
+    return this.http.get('https://api.mysportsfeeds.com/v1.2/pull/nba/current/cumulative_player_stats.json', httpOptions)
+  }
+
 }
