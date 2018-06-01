f66b36cb314fef764e1dc4afff3a8b8cc7fefc91
Szerszen1 <jakubmarekszerszen@gmail.com>
2018-06-01 18:46:32 +0200

    trying to connect with api

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -2,17 +2,15 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
 
-const httpOptions = {
-  headers: new HttpHeaders({ 'Authorization': 'Szerszen1:szerszen1' })
-};
+ const headers = new HttpHeaders().set('Szerszen1', 'szerszen1');
 
-@Injectable()
+ @Injectable()
 export class HttpService {
 
   constructor(private http: HttpClient) {}
 
   get() {
-    return this.http.get('https://api.mysportsfeeds.com/v1.2/pull/nba/current/cumulative_player_stats.json', httpOptions)
+    return this.http.get(' https://api.mysportsfeeds.com/v1.2/pull/nba/current/latest_updates.json', {headers})
   }
 
 }
