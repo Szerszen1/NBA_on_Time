948f183da73cc7c2c02b36d65da0a8b36f4b6001
Szerszen1 <jakubmarekszerszen@gmail.com>
2018-06-04 14:13:56 +0200

    trying to display players

--- src/app/http.service.ts
+++ src/app/http.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { HttpParams, HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 const _headers = new HttpHeaders();
 const headers = _headers.append('Authorization', 'Basic ' + btoa('Szerszen1' + ':' + 'szerszen1'));
@@ -13,7 +13,8 @@ export class HttpService {
   constructor(private http: HttpClient) {}
 
   get() {
-    return this.http.get(' https://api.mysportsfeeds.com/v1.2/pull/nba/current/latest_updates.json', {headers})
+    return this.http.get('  https://api.mysportsfeeds.com/v1.2/pull/nba/current/roster_players.json?fordate=20180604&team=bos ',
+     {headers})
   }
 
 }
--- src/app/teams/teams.component.ts
+++ src/app/teams/teams.component.ts
@@ -1,6 +1,6 @@
 import { HttpService } from './../http.service';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-teams',
@@ -17,8 +17,12 @@ export class TeamsComponent implements OnInit {
 
   getCos() {
     this.httpService.get().subscribe(cos => {
+      this.players = cos as Players[];
       console.log(cos)
-    });
+    }, error => console.error(error))
+  }
+  wyswietl() {
+    console.log(this.players[0].ID)
   }
 
 }
