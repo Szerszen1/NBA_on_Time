aae6cedd8d95840f86c242e2072e871c5fdfac54
ZdebK <zdeb.katarzyna@gmail.com>
2018-06-05 21:54:12 +0200

    new conception of youtube viewer

--- src/app/youtube-player/youtube-player.component.ts
+++ src/app/youtube-player/youtube-player.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'app-youtube-player',
@@ -6,10 +7,38 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./youtube-player.component.css']
 })
 export class YoutubePlayerComponent implements OnInit {
+  title ="Recent on channel";
+  someItem="<h1>Sth</h1>"
+  videoList = [
+    {
+    name: "Item 1",
+    slug: "item-1",
+    embed: `7NUTAFTljyA`,
+    },
+    {
+    name: "item 2",
+    slug: "item-2",
+    embed: `7NUTAFTljyA`,
+    },
+    {
+      name: "item 3",
+      slug:"item-3",
+      embed: null,
+    }
+  ]
+  constructor(private sanitizer: DomSanitizer) { }
 
-  constructor() { }
+  ngOnInit() {
+  }
 
-  player: YT.Player;
+  getEmbedUrl(item){
+    return this.sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/watch?v='+ item.embed)
+  }
+}
+
+
+/*  player: YT.Player;
+  channelId: 'UCWJ2lWNubArHWmf3FIHbfcQ';
   id: string = 'k6RMYTgWk-8';
 
   ngOnInit() {
@@ -22,5 +51,5 @@ export class YoutubePlayerComponent implements OnInit {
   onStateChange(event) {
     console.log('player state', event.data);
   }
+*/
 
-}
