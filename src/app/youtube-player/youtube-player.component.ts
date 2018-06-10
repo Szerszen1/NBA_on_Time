7655c4749240f747195c71954246d9db1b8f9dc0
ZdebK <zdeb.katarzyna@gmail.com>
2018-06-10 16:34:08 +0200

    youtubecomponent update

--- src/app/youtube-player/youtube-player.component.ts
+++ src/app/youtube-player/youtube-player.component.ts
@@ -8,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class YoutubePlayerComponent implements OnInit {
 
   constructor() { }
+  id: string = 'k6RMYTgWk-8';
+  id1: string = 'k6RMYTgWk-8';
+  id2: string = 'EKOJb0wv6yI';
+  id3: string = 'Crq9XLTqxOw';
+  id4: string = 'Fl0ohG57PSY';
+  id5: string = '6cyLhaqKR-c';
+  id6: string = 'mU22lWPu37k';
+  id7: string = 'XdXfQ-ubWy0';
+  id8: string = 'vdupncE8gXM';
+  id9: string = 'AUPeHXijgH8';
+  id11: string = 'Jb8RX8b3b30';
+  id12: string = 'C27hkQ5ARaY';
+  id14:string = 'qvHKVQtalas';
+  id15: string = 'exjMt3v81tA';
 
   player: YT.Player;
-  id: string = 'k6RMYTgWk-8';
+
 
   ngOnInit() {
   }
