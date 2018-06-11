8cf4b9721edf4743f789257e9ddbf76cff719e28
Daooshi <dominikwozniczka96@gmail.com>
2018-06-11 10:36:49 +0200

    solving problem with import youtube

--- src/app/teams/wizards/wizards.component.ts
+++ src/app/teams/wizards/wizards.component.ts
@@ -12,13 +12,21 @@ export class WizardsComponent implements OnInit {
 
   playerentry$: Observable<Playerentry[]>;
   teamName = 'was';
-  ww:string ='pAtDS3_dEWs';
+  ww: string = 'pAtDS3_dEWs';
   player: YT.Player;
 
   constructor(private httpService: HttpService) { }
 
   ngOnInit() {
     this.playerentry$ = this.httpService.getRoster(this.teamName);
+  }
+
+  savePlayer (player) {
+    this.player = player;
+    console.log('player instance', player);
+	}
+  onStateChange(event) {
+    console.log('player state', event.data);
   }
 
 }
